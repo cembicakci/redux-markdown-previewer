4ab1cfe77aa53116c9e920bc456c45c001351e04
Cem <cmbicakci@gmail.com>
2022-08-20 18:58:59 +0300

    added React Markdown

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,12 +3,13 @@ import ReactMarkdown from 'react-markdown'
 import './style.css'
 
 function Content() {
+
   return (
     <div className='content'>
-      <textarea placeholder='Please write anything here...' className='content-left'/>
+      <textarea placeholder='Please write anything here...' className='content-left' />
 
       <div className='content-right'>
-        <ReactMarkdown>## Hello, *world*!</ReactMarkdown>
+        <ReactMarkdown  />
       </div>
 
 
